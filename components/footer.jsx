b460d81e301c6d32fe5e32ffa1e8806caa5e8bb6
jin <huynhca.dev@gmail.com>
2025-05-18 22:48:53 +0700

    third commit

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row gap-2 justify-between px-[40px] py-[28px] mt-[20px] min-h-[180px]">
-      <div className="flex flex-col">
-        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">CONTACT</div>
+    <div className="flex flex-row gap-0 tablet:gap-2 justify-between px-[40px] py-[28px] mt-[20px] min-h-[180px] flex-wrap">
+      <div className="flex flex-col w-1/2 tablet:w-auto pr-1 tablet:pr-0">
+        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">
+          CONTACT
+        </div>
         <Link
-          className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold"
+          className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold truncate"
           href="https://mail.google.com/mail/?view=cm&fs=1&to=BETHANYNAMTRAN%40GMAIL.COM"
         >
           BETHANYNAMTRAN@GMAIL.COM
@@ -16,14 +18,16 @@ const Footer = () => {
           PHONE NUMBER
         </div>
         <Link
-          className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold"
+          className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold truncate"
           href="https://www.bethany.co.nz/tel:64211465868"
         >
           (+64) 211 465 868
         </Link>
       </div>
-      <div className="flex flex-col">
-        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">MORE INFO</div>
+      <div className="flex flex-col w-1/2 tablet:w-auto pl-1 tablet:pl-0">
+        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">
+          MORE INFO
+        </div>
         <Link
           className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold"
           href="https://www.linkedin.com/in/bethany-tran-design/?fbclid=IwAR2gap8rJgP_rZNgPIACCRGh3fwaknkddtkItuoajsmtxG6Lh81EWMosfn4"
@@ -37,14 +41,18 @@ const Footer = () => {
           RESUME
         </Link>
       </div>
-      <div className="flex flex-col">
-        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">LOCATION</div>
+      <div className="flex flex-col w-1/2 tablet:w-auto pr-1 tablet:pr-0">
+        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">
+          LOCATION
+        </div>
         <div className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold">
           TAMAKI MAKAURAU
         </div>
       </div>
-      <div className="flex flex-col">
-        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">BETHANY-TRAN</div>
+      <div className="flex flex-col w-1/2 tablet:w-auto pl-1 tablet:pl-0">
+        <div className="text-[18px] tablet:text-[22px] font-semibold mb-1">
+          BETHANY-TRAN
+        </div>
         <div className="text-[12px] tablet:text-[14px] laptop:text-[17px] font-semibold ml-auto">
           © 2024 - 2025
         </div>
